3d53fee785f2b1206d6d278e451524a44355536c
anajuliaceag <142040048+anajuliaceag@users.noreply.github.com>
2025-05-27 08:48:02 -0300

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,15 @@
+var nota1 = Number(window,prompt("Digite aqui a primeira nota:"));
+var nota2 = Number(window,prompt("Digite aqui a segunda nota:"));
+var nota3 = Number(window,prompt("Digite aqui a terceira nota:"));
+
+let total  = (nota1 + nota2 + nota3 )/ 3;
+
+alert ("Media final: " + total)
+
+ if(total >= 60){
+  alert("Você atingiu a media " + total)
+  alert("Aprovado")
+ }else{
+  pontosFaltando = 60 - total
+  alert("Reprovado faltaram" + pontosFaltando + "pontos")
+ }
